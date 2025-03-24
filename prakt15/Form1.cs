536c78b33cd694340e0c1a901f349d1e75b81682
Alexandr <86582717+tseneven@users.noreply.github.com>
2025-03-24 13:18:11 +0500

    Add files via upload

---
+++ prakt15/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace prakt15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Predmet predmet = new Predmet();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void найтиСловоToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            predmet.words = textBox1.Text;
+
+            string fileName = "txt.txt";
+
+            string fileContent = File.ReadAllText(fileName);
+
+            string[] lines = fileContent.Split(new[] {"\r\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach(string line in lines) 
+            {
+                int lastSpace = line.LastIndexOf(' ');
+                if (lastSpace == -1) continue;
+
+                string text = line.Substring(0, lastSpace);
+                string pageStr = line.Substring(lastSpace + 1);
+
+
+            }
+
+        }
+    }
+}
---
+++ prakt15/Predmet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt15
+{
+    class Predmet
+    {
+        public int number_page;
+        public string words;
+        public Dictionary<int, string> page;
+
+        // Метод поиска слова на каждой странице
+        public string Search() 
+        {
+            for(int i = 0; i < page.Count; i++)
+            {
+                var pageValue = page.ElementAt(i);
+
+                string[] words_in_page = pageValue.Value.Split(' ');
+
+                for(int y = 0; y < words_in_page.Length; y++)
+                {
+                    if(words_in_page[i] == words)
+                    {
+                        number_page = pageValue.Key;
+                        return $"Слово на странице {number_page}" ;
+                    }
+                }
+            }
+            return "Такого слова нету в тексте";
+        }
+    }
+}
